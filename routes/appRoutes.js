2d8a514045dd642d0b406d86a2a1fca3e4bc165f
steveyoss <steve@yoss.io>
2020-02-24 22:36:06 -0800

    Added some basic views including partials for head, header and footer.

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,12 +2,25 @@
 module.exports = function(app) {
   var user = require("../controller/appController.js");
 
-  // User Routes
-  app.route('/users')
+  // Default Route
+  app.get("/", function(req, res) {
+    res.render("pages/index");
+  });
+
+// about page 
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+  // API STUFF 
+  // API User Routes
+  app
+    .route("/users")
     .get(user.list_all_users)
     .post(user.create_user);
 
-  app.route('/users/:userId')
+  app
+    .route("/users/:userId")
     .get(user.getById)
     .delete(user.deleteById)
     .put(user.updateById);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   app = express(),
   bodyParser = require('body-parser');
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 8080;
 
 
 const mysql = require('mysql');
@@ -18,10 +18,14 @@ mc.connect();
 
 app.listen(port);
 
-console.log('API server started on: ' + port);
+console.log('Enter the Matrix on : ' + port);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var routes = require('./routes/approutes'); //importing route
 routes(app); //register the route
+
+//set default engine, and provide [handlebars as] extension
+app.set('view engine', 'ejs');
+app.set("views", "./views");
